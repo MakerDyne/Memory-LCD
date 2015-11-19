81c4e9e0d969b3bca9952f09d3fa339a9bc2839b
Richard Leszczynski <richard@makerdyne.com>
2015-11-19 14:59:40 +0000

    first draft of ColourLinebuffer class

--- Linebuffer.cpp
+++ Linebuffer.cpp
@@ -6,6 +6,11 @@
  #include "WProgram.h"
 #endif
 
+/*
+ * MonoLinebuffer class methods
+ */
+
+
 MonoLinebuffer::MonoLinebuffer() {
   //  
 }
@@ -13,6 +18,7 @@ MonoLinebuffer::MonoLinebuffer() {
 
 bool MonoLinebuffer::writePixelToBuffer(unsigned int pixelPosition, bool isWhite) {
   if(pixelPosition < lcdPixelWidth) {
+    // TODO: replace Arduino bitWrite function to make code more portable
     bitWrite(buffer[pixelPosition/8], 7 - pixelPosition%8, isWhite);
     return true;
   }
@@ -53,5 +59,93 @@ void MonoLinebuffer::setBufferWhite() {
 
 
 const char * const MonoLinebuffer::getPointerToBuffer() {
+  return pBuffer;
+}
+
+
+/* 
+ * ColourLinebuffer class methods
+ */
+
+
+ColourLinebuffer::ColourLinebuffer() {
+  //  
+}
+
+
+bool ColourLinebuffer::writePixelToBuffer(unsigned int pixelPosition, colour c) {
+  if((c < 8) && (pixelPosition < lcdPixelWidth)) {
+    unsigned int currentBit;	// for a 128px LCD, value should be in range 0-383
+    unsigned int currentByte;	// for a 128px LCD, value should be in range 0-127
+    byte bitWithinByte;	// position of currentBit within the currentByte, value in range 0-7
+    for(byte i=0; i<3; i++) {
+      currentBit = (pixelPosition*3) + i;
+      currentByte = currentBit/8;
+      bitWithinByte = 7-(currentBit%8);
+      if( (c & (1 < i)) == 0 ) {
+	// set bit to zero
+	buffer[currentByte] &= ~(1 << bitWithinByte);
+      }
+      else {
+	// set bit to one
+	buffer[currentByte] |= (1 << bitWithinByte);
+      }
+    }
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+
+bool writePixelToBuffer(unsigned int pixelPosition, bool isRed, bool isGreen, bool isBlue) {
+  if(pixelPosition < lcdPixelWidth) {
+    byte RGB = 0;
+    if(isBlue)
+      RGB = 1;
+    if(isGreen)
+      RGB |= (1 << 1);
+    if(isRed)
+      RGB |= (1 << 2);
+    return writePixelToBuffer(pixelPosition, RGB);
+  }
+  else {
+    return false;
+  }
+}
+
+
+// Commented out pending deletion as pixels cross over byte boundaries. 
+// bool ColourLinebuffer::writeByteToBuffer(byte bytePosition, byte byteContents) {
+//   if(bytePosition < bufferSize) {
+//     buffer[bytePosition] = byteContents;
+//     return true;
+//   }
+//   else {
+//     return false;
+//   }
+// }
+
+
+void MonoLinebuffer::clearBuffer() {
+  for(byte i=0; i<lcdPixelWidth/8; i++) {
+    buffer[i] = 0xFF;
+  }
+}
+
+
+void setBufferColour(colour c) {
+  // TODO: individually Set the first 8 pixels (24 bits, 3 bytes) 
+  // then copy the whole bytes to the rest of the linebuffer for speed?
+  for(unsigned int i=0; i<lcdPixelWidth; i++) 
+    writePixelToBuffer(i, c);
+  }
+}
+  
+
+
+
+const char * const ColourLinebuffer::getPointerToBuffer() {
   return pBuffer;
 }
--- Linebuffer.h
+++ Linebuffer.h
@@ -57,4 +57,38 @@ private:
   char * const pBuffer = buffer; 
 };
 
+
+class ColourLinebuffer {
+public:     
+  enum colour {	// RGB
+    black,	// 000
+    blue,	// 001
+    green,	// 010
+    cyan,	// 011
+    red,	// 100
+    magenta,	// 101
+    yellow,	// 110
+    white 	// 111
+  };
+  ColourLinebuffer();
+  bool writePixelToBuffer(unsigned int pixelPosition, colour c);
+  bool writePixelToBuffer(unsigned int pixelPosition, bool isRed, bool isGreen, bool isBlue);
+//  bool writeByteToBuffer(byte bytePosition, byte byteContents);
+  void clearBuffer();
+  void setBufferColour(colour c);
+  const char * const getPointerToBuffer();
+private:
+  unsigned int lcdPixelWidth = 128;
+  char bufferSize = 48;
+  char buffer[bufferSize] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
+			     0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
+			     0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 
+			     0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF };                  
+
+/*
+ * END EDITING - COMMENT OR UNCOMMENT TO SUIT YOUR MODEL OF MEMORY LCD
+*/  
+  char * const pBuffer = buffer; 
+};
+
 #endif
